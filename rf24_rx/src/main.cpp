40c8f158d48ca695853a0674dbc6ee151cee5aef
nquangtrung <nquangtrung@gmail.com>
2026-06-17 15:35:53 +0200

    Read faster

--- rf24_rx/src/main.cpp
+++ rf24_rx/src/main.cpp
@@ -6,12 +6,6 @@
 #include "UI.h"
 #include "receiver.h"
 
-// 1. Declare a custom pointer for your SPI bus
-// SPIClass *vvspi = NULL;
-
-// 2. Pass the pins and the custom speed (4MHz) into the radio constructor
-// RF24 r(CE_PIN, CSN_PIN, 4000000);
-
 TaskHandle_t UITaskHandle = NULL;
 TaskHandle_t ReceiverTaskHandle = NULL;
 
@@ -30,7 +24,7 @@ void ReceiverTask(void *pvParameters)
   while (1)
   {
     Radio_loop();
-    vTaskDelay(1000 / portTICK_PERIOD_MS); // Adjust the delay as needed
+    vTaskDelay(500 / portTICK_PERIOD_MS); // Adjust the delay as needed
   }
 }
 
